87a87039804b4eb5e14b995cce27ce424f62ae90
Toheeb Ashorobi <tasho646fr@gmail.com>
2023-06-01 10:33:56 +0200

    recruiter route done

--- routes/recruiter.routes.js
+++ routes/recruiter.routes.js
@@ -20,4 +20,50 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const oneRecuriter = await Recruiter.findById(id);
+    console.log("Does this work?", oneRecuriter);
+    res.json(oneRecuriter);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allRecruiters = await Recruiter.find();
+    res.json(allRecruiters);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { firstName, lastName, email } = req.body;
+  try {
+    const updateRecruiter = await Recruiter.findByIdAndUpdate(
+      id,
+      { firstName, lastName, email },
+      { new: true }
+    );
+    console.log("Test to see if it works:", updateRecruiter);
+    res.json(updateRecruiter);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deleteRecruiter = await Recruiter.findByIdAndDelete(id);
+    res.json({ message: "The user has be deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
